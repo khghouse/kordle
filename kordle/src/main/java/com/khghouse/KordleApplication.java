1fc515463d85c91f41cdd0f61ab962eab5156f84
khghouse <khghouse@naver.com>
2023-03-06 21:23:13 +0900

    init

---
+++ kordle/src/main/java/com/khghouse/KordleApplication.java
@@ -0,0 +1,13 @@
+package com.khghouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KordleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KordleApplication.class, args);
+    }
+
+}
---
+++ kordle/src/test/java/com/khghouse/KordleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khghouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KordleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
